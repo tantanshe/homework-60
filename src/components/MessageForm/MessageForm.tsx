7a9d861ae3627f601fcdddcb2bb8113d257c2060
Tatiana Shishenina <tatyana020711@gmail.com>
2024-06-16 21:25:53 +0600

    Added styles and finished the app

--- src/components/MessageForm/MessageForm.tsx
+++ src/components/MessageForm/MessageForm.tsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {Form, Button, Container} from 'react-bootstrap';
 
 interface MessageFormProps {
   onSendMessage: (message: string, author: string) => void;
@@ -18,21 +19,31 @@ const MessageForm: React.FC<MessageFormProps> = ({onSendMessage}) => {
   };
 
   return (
-    <form onSubmit={submitMessage}>
-      <input
-        type="text"
-        value={author}
-        onChange={(e) => setAuthor(e.target.value)}
-        placeholder="Type in your name"
-      />
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-        placeholder="Type in your message"
-      />
-      <button type="submit">Send</button>
-    </form>
+    <Container className="mt-3">
+      <Form onSubmit={submitMessage}>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            placeholder="Type in your name"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            placeholder="Type in your message"
+          />
+        </Form.Group>
+        <div className="d-grid gap-2">
+          <Button variant="primary" size="lg" type="submit">
+            Send
+          </Button>
+        </div>
+      </Form>
+    </Container>
   );
 };
 
--- src/components/MessagesList/MessagesList.tsx
+++ src/components/MessagesList/MessagesList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Message} from '../../types';
+import {Container, Card} from 'react-bootstrap';
 
 interface MessagesProps {
   messages: Message[];
@@ -7,14 +8,17 @@ interface MessagesProps {
 
 const MessagesList: React.FC<MessagesProps> = ({messages}) => {
   return (
-    <div>
-      {messages.map(message => (
-        <div key={message._id}>
-          <p>{message.author}: {message.message}</p>
-          <p>{(message.datetime).toLocaleString()}</p>
-        </div>
+    <Container className="mt-4">
+      {messages.map((message) => (
+        <Card key={message._id} className="border border-dark rounded mt-3 mb-3" bg="light">
+          <Card.Body className="p-3">
+            <Card.Title className="font-weight-bold">{message.author}</Card.Title>
+            <Card.Text className="mt-1">{message.message}</Card.Text>
+            <Card.Text className="text-muted mb-0">{new Date(message.datetime).toLocaleString()}</Card.Text>
+          </Card.Body>
+        </Card>
       ))}
-    </div>
+    </Container>
   );
 };
 

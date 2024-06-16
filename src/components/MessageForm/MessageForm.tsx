252fd333b0162eb803352bf0d7987ba681ddb056
Tatiana Shishenina <tatyana020711@gmail.com>
2024-06-16 16:20:46 +0600

    Created MessageForm component to handle logic for sending messages

---
+++ src/components/MessageForm/MessageForm.tsx
@@ -0,0 +1,39 @@
+import React, {useState} from 'react';
+
+interface MessageFormProps {
+  onSendMessage: (message: string, author: string) => void;
+}
+
+const MessageForm: React.FC<MessageFormProps> = ({onSendMessage}) => {
+  const [message, setMessage] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const submitMessage = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (message && author) {
+      onSendMessage(message, author);
+      setMessage('');
+      setAuthor('');
+    }
+  };
+
+  return (
+    <form>
+      <input
+        type="text"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        placeholder="Type in your name"
+      />
+      <input
+        type="text"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        placeholder="Type in your message"
+      />
+      <button type="submit" onSubmit={submitMessage}>Send</button>
+    </form>
+  );
+};
+
+export default MessageForm;
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
+import './App.css';
 
 const App = () => {
   return (
     <>
       App
     </>
-  )
+  );
 };
 
-export default App
+export default App;
